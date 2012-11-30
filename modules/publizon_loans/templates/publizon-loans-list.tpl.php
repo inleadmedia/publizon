0e29596c0a61af8a7a78fad5456fe88db85f17c2
Jesper Kristensen <cableman@linuxdev.dk>
2012-11-30 15:13:20 +0100

    Added string when user does not have any loans

--- modules/publizon_loans/templates/publizon-loans-list.tpl.php
+++ modules/publizon_loans/templates/publizon-loans-list.tpl.php
@@ -5,7 +5,11 @@
  */
 ?>
 <ul class="publizon-loans-list">
-  <?php foreach ($loans as $loan): ?>
-    <li class="display-book biglist"><?php echo theme('publizon_loan', $loan); ?></li>
-  <?php endforeach; ?>
+  <?php if (!empty($loans)) { ?>
+    <?php foreach ($loans as $loan): ?>
+      <li class="display-book biglist"><?php echo theme('publizon_loan', $loan); ?></li>
+    <?php endforeach; ?>
+  <?php } else { ?>
+      <li><?php echo t('You don\'t have any active loans.') ?></li>
+  <?php } ?>
 </ul>
